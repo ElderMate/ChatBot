890769cda956f608b11146e0cece652c9dae462d
JangWooJin1 <jangwoojin1@gmail.com>
2024-06-01 00:16:54 +0900

    문자 분류 모델 제거

--- ChatBot_server.py
+++ ChatBot_server.py
@@ -9,7 +9,6 @@ from datafile.datamodel import PromptResponse, makeMsgData, ProgressModel, endMo
 from pydantic import BaseModel
 from transformers import AutoModelForTokenClassification, AutoTokenizer
 import torch
-import joblib
 import numpy as np
 
 app = FastAPI()
@@ -108,10 +107,6 @@ class Item(BaseModel):
 
 # 각 클래스별 모델과 id2tag 설정
 models_info = {
-    "payment_denial": {
-        "model_path": "kobert_결제거절",
-        "id2tag": {0: 'O', 1: 'METHOD', 2: 'LOCATION', 3: 'TIME', 4: 'COST', 5: "REJECTION_REASON"}
-    },
     "payment_approval": {
         "model_path": "kobert_결제승인",
         "id2tag": {0: 'O', 1: 'METHOD', 2: 'LOCATION', 3: 'TIME', 4: 'COST'}
@@ -127,10 +122,6 @@ models_info = {
     "payment_scheduled": {
         "model_path": "kobert_납부예정",
         "id2tag": {0: 'O', 1: 'PAYEE', 2: 'COST', 3: 'TIME', 4: 'PAYMENT REASON'}
-    },
-    "non_payment": {
-        "model_path": "kobert_미납",
-        "id2tag": {0: 'O', 1: 'PAYEE', 2: 'COST', 3: 'TIME'}
     },
     "automatic_transfer": {
         "model_path": "kobert_자동이체",
@@ -199,43 +190,3 @@ async def create_item(class_name: str, item: Item):
         return extracted_info
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-
-
-
-# 랜덤 포레스트 모델과 TF-IDF 벡터라이저 로드
-model_path = './model/random_forest_model.pkl'
-vectorizer_path = './vectorizer/tfidf_vectorizer.joblib'
-model = joblib.load(model_path)
-vectorizer = joblib.load(vectorizer_path)
-
-index_to_label = {
-    0: '납부 예정',
-    1: '결제 거절',
-    2: '결제 승인',
-    3: '자동 이체',
-    4: '미납',
-    5: '결제 취소',
-    6: '계좌 개설'
-}
-
-def get_label_from_index(index):
-    return index_to_label.get(index, "Unknown")
-
-def preprocess_text2(text):
-    # 텍스트를 TF-IDF 벡터로 변환
-    vectorized_text = vectorizer.transform([text])
-    return vectorized_text
-
-class TextItem(BaseModel):
-    text: str
-
-@app.post("/class/")
-async def predict(item: TextItem):
-    try:
-        preprocessed_text = preprocess_text2(item.text)
-        prediction = model.predict(preprocessed_text)[0]
-        predicted_index = np.argmax(prediction)
-        return {"response": get_label_from_index(predicted_index)}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-
